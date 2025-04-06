c6b778edc55fb6204d78babd4c37602b5fcd6216
Franco Armando <francoarmando1911@gmail.com>
2025-04-06 19:59:27 -0300

    Actualizacion de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
+  standalone: true, 
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] 
 })
 export class AppComponent {
   title = 'totalimp-angular';
 }
+
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isMenuOpen = false;
+  isSubmenuOpen = false;
+  isDesktopView = false;
+
+  ngOnInit(): void {
+    this.updateViewMode();
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+    if (!this.isMenuOpen) this.isSubmenuOpen = false;
+  }
+
+  toggleSubmenu(): void {
+    if (!this.isDesktopView) {
+      this.isSubmenuOpen = !this.isSubmenuOpen;
+    }
+  }
+
+  @HostListener('window:resize', [])
+  onResize(): void {
+    this.updateViewMode();
+  }
+
+  private updateViewMode(): void {
+    if (typeof window !== 'undefined') {
+      this.isDesktopView = window.innerWidth > 768;
+    }
+  }
+
+  onMouseEnter(): void {
+    if (this.isDesktopView) {
+      this.isSubmenuOpen = true;
+    }
+  }
+
+  onMouseLeave(): void {
+    if (this.isDesktopView) {
+      this.isSubmenuOpen = false;
+    }
   }
 }
